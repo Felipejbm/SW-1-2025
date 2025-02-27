b3b85828c1407ef7dde15bff7ba1e10de0558be5
Felipe José Borges de Mello <159478861+Felipejbm@users.noreply.github.com>
2025-02-27 08:57:30 -0300

    Add files via upload

---
+++ aula03/processa01.php
@@ -0,0 +1,7 @@
+<?php
+    $nome = $_REQUEST['cxnome'];
+    $idade = $_REQUEST['cxidade'];
+
+    echo"Olá $nome de $idade anos de idade";
+?>
+
---
+++ aula03/processa_exe01.php
@@ -0,0 +1,7 @@
+<?php
+    $nome = $_POST["cxnome"];
+    $msg = $_POST["cxopin"];
+
+    echo"Obrigado por seu comentário $nome! <br>";
+echo"Seu comentário foi: $msg <br>";
+?>
